856c6ad713240e495f34598430d3cd6288b64b5e
hirnworx <74244303+hirnworx@users.noreply.github.com>
2024-12-12 15:00:56 +0100

    Update pricing_logic_lightbox.py

--- pricing_logic_lightbox.py
+++ pricing_logic_lightbox.py
@@ -1,178 +1,32 @@
 def lk_price(qcm, type, lkw):
-    if qcm < 1500:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 950
-            elif type == "beitseitig":
-                return 1045
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 1370
-            elif type == "beitseitig":
-                return 1580
-    elif qcm < 3000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 950
-            elif type == "beitseitig":
-                return 1045
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 1370
-            elif type == "beitseitig":
-                return 1580
-    elif qcm < 4000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 665
-            elif type == "beitseitig":
-                return 730
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 970
-            elif type == "beitseitig":
-                return 1115
-    elif qcm < 5000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 665
-            elif type == "beitseitig":
-                return 730
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 970
-            elif type == "beitseitig":
-                return 1115
-    elif qcm < 6000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 570
-            elif type == "beitseitig":
-                return 620
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 820
-            elif type == "beitseitig":
-                return 950
-    elif qcm < 7500:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 510
-            elif type == "beitseitig":
-                return 560
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 745
-            elif type == "beitseitig":
-                return 855
-    elif qcm < 10000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 450
-            elif type == "beitseitig":
-                return 495
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 660
-            elif type == "beitseitig":
-                return 760
-    elif qcm < 12500:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 425
-            elif type == "beitseitig":
-                return 475
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 630
-            elif type == "beitseitig":
-                return 725
-    elif qcm < 15000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 415
-            elif type == "beitseitig":
-                return 460
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 610
-            elif type == "beitseitig":
-                return 700
-    elif qcm < 17500:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 395
-            elif type == "beitseitig":
-                return 440
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 580
-            elif type == "beitseitig":
-                return 670
-    elif qcm < 20000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 385
-            elif type == "beitseitig":
-                return 425
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 560
-            elif type == "beitseitig":
-                return 650
-    elif qcm < 30000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 380
-            elif type == "beitseitig":
-                return 415
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 550
-            elif type == "beitseitig":
-                return 630
-    elif qcm < 40000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 350
-            elif type == "beitseitig":
-                return 385
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 515
-            elif type == "beitseitig":
-                return 595
-    elif qcm < 50000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 335
-            elif type == "beitseitig":
-                return 370
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 495
-            elif type == "beitseitig":
-                return 570
-    elif qcm < 59999:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 325
-            elif type == "beitseitig":
-                return 360
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 475
-            elif type == "beitseitig":
-                return 550
-    elif qcm >= 60000:
-        if lkw == 'scheibe':
-            if type == "einseitig":
-                return 315
-            elif type == "beitseitig":
-                return 350
-        elif lkw == 'dk':
-            if type == "einseitig":
-                return 460
-            elif type == "beitseitig":
-                return 530
+    # Define the price table for ranges and prices
+    price_table = [
+        (1500, {"scheibe": {"einseitig": 950, "beitseitig": 1045}, "dk": {"einseitig": 1370, "beitseitig": 1580}}),
+        (3000, {"scheibe": {"einseitig": 950, "beitseitig": 1045}, "dk": {"einseitig": 1370, "beitseitig": 1580}}),
+        (4000, {"scheibe": {"einseitig": 665, "beitseitig": 730}, "dk": {"einseitig": 970, "beitseitig": 1115}}),
+        (5000, {"scheibe": {"einseitig": 665, "beitseitig": 730}, "dk": {"einseitig": 970, "beitseitig": 1115}}),
+        (6000, {"scheibe": {"einseitig": 570, "beitseitig": 620}, "dk": {"einseitig": 820, "beitseitig": 950}}),
+        (7500, {"scheibe": {"einseitig": 510, "beitseitig": 560}, "dk": {"einseitig": 745, "beitseitig": 855}}),
+        (10000, {"scheibe": {"einseitig": 450, "beitseitig": 495}, "dk": {"einseitig": 660, "beitseitig": 760}}),
+        (12500, {"scheibe": {"einseitig": 425, "beitseitig": 475}, "dk": {"einseitig": 630, "beitseitig": 725}}),
+        (15000, {"scheibe": {"einseitig": 415, "beitseitig": 460}, "dk": {"einseitig": 610, "beitseitig": 700}}),
+        (17500, {"scheibe": {"einseitig": 395, "beitseitig": 440}, "dk": {"einseitig": 580, "beitseitig": 670}}),
+        (20000, {"scheibe": {"einseitig": 385, "beitseitig": 425}, "dk": {"einseitig": 560, "beitseitig": 650}}),
+        (30000, {"scheibe": {"einseitig": 380, "beitseitig": 415}, "dk": {"einseitig": 550, "beitseitig": 630}}),
+        (40000, {"scheibe": {"einseitig": 350, "beitseitig": 385}, "dk": {"einseitig": 515, "beitseitig": 595}}),
+        (50000, {"scheibe": {"einseitig": 335, "beitseitig": 370}, "dk": {"einseitig": 495, "beitseitig": 570}}),
+        (59999, {"scheibe": {"einseitig": 325, "beitseitig": 360}, "dk": {"einseitig": 475, "beitseitig": 550}}),
+        (75000, {"scheibe": {"einseitig": 315, "beitseitig": 350}, "dk": {"einseitig": 460, "beitseitig": 530}}),
+        (100000, {"scheibe": {"einseitig": 305, "beitseitig": 340}, "dk": {"einseitig": 445, "beitseitig": 515}}),
+        (125000, {"scheibe": {"einseitig": 295, "beitseitig": 330}, "dk": {"einseitig": 430, "beitseitig": 500}}),
+        (150000, {"scheibe": {"einseitig": 285, "beitseitig": 320}, "dk": {"einseitig": 415, "beitseitig": 485}}),
+        (200000, {"scheibe": {"einseitig": 275, "beitseitig": 310}, "dk": {"einseitig": 400, "beitseitig": 470}}),
+    ]
+
+    # Determine the price based on qcm and the ranges
+    for max_qcm, prices in price_table:
+        if qcm <= max_qcm:
+            return prices.get(lkw, {}).get(type, 0)
+
+    # Default return if no match found
     return 0
